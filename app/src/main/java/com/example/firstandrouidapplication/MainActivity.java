d9dba7758daf73825f407d4e3b357b938ccda7f8
ilya <011283im@gmail.com>
2022-03-26 14:47:49 +0300

    всторая и 3я часть задания

--- app/src/main/java/com/example/firstandrouidapplication/MainActivity.java
+++ app/src/main/java/com/example/firstandrouidapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.firstandrouidapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.linear_button_task2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_task2);
+            }
+        });
     }
 }
